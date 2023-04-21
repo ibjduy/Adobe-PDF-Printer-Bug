566710f2561a9046eb18180272b2980fe3fa96e5
ibjduy <122287999+ibjduy@users.noreply.github.com>
2023-04-21 09:40:45 -0400

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using RawPrint;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"AAA.ps";
+            var printerName = "Adobe PDF";//"CutePDF Writer"; //other printers work
+            var printer = new Printer();
+            printer.PrintRawFile(printerName, path, false);
+        }
+    }
+}
